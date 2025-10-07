590f9ed00394a603df7dec9454de8b4bf194aedc
Tanay-22 <144043275+Tanay-22@users.noreply.github.com>
2025-10-07 15:39:08 +0530

    gRPC learning

---
+++ protobuf-demo/src/test/java/com/tanay/protobufdemo/ProtobufDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tanay.protobufdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtobufDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
